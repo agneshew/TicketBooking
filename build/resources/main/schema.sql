97fb5239d829877a4e6b381e2e0fa646ef9f8f8e
agneshew <agnes.hewusz@gmail.com>
2021-02-17 21:58:11 +0100

    Liquibase config

---
+++ build/resources/main/schema.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+--changeset author: agnes
+CREATE TABLE `users` (
+    `email` VARCHAR(254) NOT NULL PRIMARY KEY,
+    `passwordHash` VARCHAR(68) NOT NULL,
+    `token` VARCHAR(68) NULL,
+    `active` BOOL NOT NULL DEFAULT 0,
+    `deleted` BOOL NOT NULL DEFAULT 0
+);
